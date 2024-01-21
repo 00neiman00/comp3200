7d44b916f9bee18ecec40716dae9ab214a0e00c6
00neimen00 <jongyun@seoulrobotics.org>
2024-01-21 14:42:32 +0900

    Constructor and initializer list

---
+++ section8/Vector.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Jongyun Ha on 1/21/24.
+//
+
+#include "Vector.h"
+
+Vector::Vector() : mX(0), mY(0) {
+
+}
+
+Vector::Vector(int x, int y) : mX(x), mY(y) {
+
+}
---
+++ section8/Vector.h
@@ -0,0 +1,22 @@
+//
+// Created by Jongyun Ha on 1/21/24.
+//
+
+#ifndef COMP3200_VECTOR_H
+#define COMP3200_VECTOR_H
+
+
+class Vector {
+
+public:
+    Vector();
+
+    Vector(int x, int y);
+
+private:
+    int mX;
+    int mY;
+};
+
+
+#endif //COMP3200_VECTOR_H
